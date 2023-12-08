13bfc9f0309dcee97d43c3be9f81bfd3b959ba46
Venom Vortex <family.osman786@gmail.com>
2023-12-08 17:15:06 +0200

    logo still to be edited in figma or inkscape. formhandler sorted out, incorrect mail name used in php file. formatting for email fixed.

--- contactform.php
+++ contactform.php
@@ -2,13 +2,13 @@
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
+    $mailFrom = $_POST['mail'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
     $mailTo = "info@alhashmiinstitute.co.za";
     $headers = "From: ".$mailFrom;
-    $txt = "New mail on alhashmiinstitute.co.za website form from ".$name.".\n\n".$message;
+    $txt = "New mail on alhashmiinstitute.co.za website form "."\n\n"."Name: ".$name."\n\n"."Email: ".$mailFrom."\n\n"."Subject: ".$subject."\n\n"."Message: ".$message;
 
     mail($mailTo, $subject, $txt, $headers);
     header("Location: success.html");
